a4159c7a4b8c56f6de4d5d44a2f257ad306e3244
Diego C <a01227808@gmail.com>
2023-08-03 16:53:55 -0700

    ssl options

--- backend/database.js
+++ backend/database.js
@@ -3,4 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE);
+export const sequelize = new Sequelize(process.env.DATABASE,
+    {
+        dialect: 'postgres',
+        dialectoptions: {
+            ssl: true
+        }
+    }
+);
