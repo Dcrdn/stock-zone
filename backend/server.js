57f6aef008faf1381523de976178f5cef028ffde
Diego C <a01227808@gmail.com>
2023-08-03 16:45:31 -0700

    adding session updates

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,9 @@ app.use(
         saveUninitialized: false,
         store: sessionStore,
         cookie: {
-            sameSite: false,
-            secure: false,
+            sameSite: 'none',
+            secure: true,
+            httpOnly: true,
             expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000) // 1 year in milliseconds
         }
     })
